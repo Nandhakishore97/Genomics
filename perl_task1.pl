575c8ff433029bc8136e9bb5c14408405c003657
Genomics-Hackbio <88309195+Genomics-Hackbio@users.noreply.github.com>
2021-08-04 11:06:17 +0530

    Update perl_task1.pl

--- perl_task1.pl
+++ perl_task1.pl
@@ -5,5 +5,5 @@ $email = "nandhakishorekj97@gmail.com";
 $slack_usrname = "@ Nandha";
 
 print "\n*************************************************\n";
-print("\tName            :  $name\n\tE-mail          :  $email\n\tSlack user name :  $slack_usrname");
+print("\tName            :  $name\n\tE-mail          :  $email\n\tSlack user name :  $slack_usrnamenn");
 print "\n*************************************************\n";
